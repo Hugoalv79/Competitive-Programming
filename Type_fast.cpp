b14e05db13a55b5e9883ab81f8c4b29521999962
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-09-23 15:41:06 -0500

    Add files via upload

---
+++ Type_fast.cpp
@@ -0,0 +1,13 @@
+#include <algorithm> // if you have problems with this C++ code,
+#include <cmath> // consult your programming text books first...
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+
