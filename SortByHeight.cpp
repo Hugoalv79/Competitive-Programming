2c807c595375118cc9b0c796baeebb69e20a0f19
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-10-24 00:34:12 -0500

    Add files via upload

--- SortByHeight.cpp
+++ SortByHeight.cpp
@@ -1,17 +1,34 @@
+// #include<vector>
+// #include<algorithm>
+
+// using namespace std;
+
+// vector<int> sortByHeight(vector<int> a) 
+// {
+//     vector<int>perfect_mirror=a; sort(perfect_mirror.begin(),perfect_mirror.end());
+    
+//     int paso = 0;
+//     for(int i: perfect_mirror) 
+//     {   if(i>=0) 
+//             {while(a[paso]==-1) {++paso;} //Evitar los árboles
+//             a[paso] = i;  
+//             ++paso;}
+//     }
+    
+//     return a;
+// }
 #include<vector>
 #include<algorithm>
-
 using namespace std;
-
-vector<int> sortByHeight(vector<int> a) 
-{
-    vector<int>tmp=a; sort(tmp.begin(),tmp.end());
+std::vector<int> order(std::vector<int> a) {
+    vector<int> tmp = a;
+    sort(tmp.begin(),tmp.end());
     
     int idx = 0;
     for(int i: tmp) {
         if(i>=0) {
             while(a[idx]==-1)
-                ++idx;
+                {++idx;}
             a[idx] = i;
             ++idx;
         }
