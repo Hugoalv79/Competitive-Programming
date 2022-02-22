5d867e725f10a0db305db401a5920de7e6a9a2ee
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2022-02-22 10:24:02 -0600

    Create PiWithNDigits.cpp

---
+++ tricks/PiWithNDigits.cpp
@@ -0,0 +1,12 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int N; scanf("%d",&N);
+    printf("%.*lf\n", N, M_PI); 
+    return 0;
+}
+
