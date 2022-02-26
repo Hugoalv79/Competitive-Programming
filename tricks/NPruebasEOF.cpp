b157df1a669d87df6c36d233fb38993bec52caba
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2022-02-26 11:06:13 -0600

    Add files via upload

---
+++ tricks/NPruebasEOF.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    int TC;
+    scanf("%d", &TC); //Número de casos de prueba
+    while(TC--){
+        int a,b; scanf("%d %d", &a, &b);
+        printf("%d\n", a + b); // Calcular al vuelo
+    }
+    return 0;
+}
+
+
