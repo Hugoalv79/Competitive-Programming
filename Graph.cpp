f5f2a3e0ce43410218702da8a3a5282ceb3ce734
Hugo Alvarado <79931043+Hugoalv79@users.noreply.github.com>
2022-10-14 01:52:19 -0500

    Add files via upload

--- Graph.cpp
+++ Graph.cpp
@@ -1,37 +1,57 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-//                             DFS
-/* Steven Halim -> Recursive
-Esta versión utiliza un vector de entero global: vi dfs_num para distinguir entre
+
+typedef vector<int> vi;
+typedef vector<bool> vb;
+
+enum{ UNVISITED = -1, VISITED = -2}; // Etiquetas básicas
+
+vi dfs_num; /*  No visitados
+
+Si tu intención es utilizar la DFS en forma básica, puedes modificar el código y cambiar vi dfs_enum por 
+    vb visited
+
+AL = Adjacency List -> La estructura de datos más eficiente para el recorrido de grafos
+AM = Adjacency Matrix
+EL = Edges List
+Puede resultar beneficioso empezar convirtiendo los grafos de entrada con forma AM o EL a AL antes de comenzar
+el recorrido.
 */
+void dfs(int u){
+    dfs_num[u] = VISITED;
+    for(auto& [v,w]: AL[u])
+        if (dfs_num[v] == UNVISITED)
+            dfs(v);
+}
 
 
-// Indian professor
 
 
-
-//   BFS
-void BFS(int vtx, int A[][8], int n){
-    queue<int> Q;
-    int visited[8] {0};
- 
-    // Initial
-    cout << vtx << ", " << flush;  // Visit vertex
-    visited[vtx] = 1;
-    Q.emplace(vtx);
- 
-    // Explore
-    while (!Q.empty()){
-        int u = Q.front();  // Vertex u for exploring
-        Q.pop();
-        for (int v = 1; v <= n; ++v){  // Adjacent vertices of vertex u
-            if (A[u][v] == 1 && visited[v] == 0){  // Adjacent vertex and not visited
-                cout << v << ", " << flush;  // Visit vertex
-                visited[v] = 1;
-                Q.emplace(v);
-            }
+class Graph{
+    int V;
+    list <int> *l;
+    public:
+        Graph(int v){
+            V = v;
+            l = new list<int> [V];
         }
+
+    void addEdge(int i,int j,bool undir = true){
+        l[i].push_back(j);
+        if(undir){
+            l[j].push_back(i);
+        }
+
     }
-    printf("\n");
+
+};
+    
+ // namespace std;
+
+int main(){
+    Graph(6);
+
+
+    return 0;
 }
